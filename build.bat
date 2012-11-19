09d18294052ca69e2ac334efa83bca6d84069dad
Tristan Seifert <tristan@squee.co>
2012-11-19 13:42:41 -0600

    Fixed build script to work on Windows.

--- build.bat
+++ build.bat
@@ -1,10 +1,14 @@
-del -f ANTIPATHY.ISO
+@echo off
+
+del SCDTEST_US.ISO
+del SCDTEST_EU.ISO
+del SCDTEST_JP.ISO
 
 REM Assemble the module loader
-bin/asm68k /o op+ /o os+ /o ow+ /o oz+ /o oaq+ /o osq+ /o omq+ /p /o ae- common/loadermodule.asm, common/loadermodule.bin, , common/loadermodule.lst
+bin\asm68k /o op+ /o os+ /o ow+ /o oz+ /o oaq+ /o osq+ /o omq+ /p /o ae- common/loadermodule.asm, common/loadermodule.bin, , common/loadermodule.lst
 
 REM Assemble modules (add more assembler commands below)
-bin/asm68k /o op+ /o os+ /o ow+ /o oz+ /o oaq+ /o osq+ /o omq+ /p /o ae- menu/menu.asm, MENU.BIN, , menu/menu.lst
+bin\asm68k /o op+ /o os+ /o ow+ /o oz+ /o oaq+ /o osq+ /o omq+ /p /o ae- menu/menu.asm, MENU.BIN, , menu/menu.lst
 
 perl scdmake_win
 pause
